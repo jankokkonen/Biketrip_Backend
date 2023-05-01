33833c2d769cc04d31820e38d92206e83a170587
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-05-01 11:52:31 +0300

    Add express and bodyparser to index.js. Add cors settings.

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// const getRoutes = require()
+// const errorController = require()
+
+const app = express();
+const ports = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
+
+// const pool = require()
+
+// app.use('/get', getRoutes);
+
+// app.use(errorController);
+
+app.listen(ports, () => console.log(`listening on port ${ports}`));
