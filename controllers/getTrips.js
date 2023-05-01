b67aa65d1f7eaa03597cc0369a14444004b947eb
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-05-01 13:23:38 +0300

    Add fetchAll function which fetch all from trips table to const rows

--- controllers/getTrips.js
+++ controllers/getTrips.js
@@ -1,1 +1,12 @@
-// const tripsModel = require();
+// const tripsModel = require('../models/getTrip');
+
+// const db = require('');
+
+exports.fetchAll = async (req, res, next) => {
+    try {
+        const { rows } = await db.query('SELECT * FROM trips');
+        res.status(200).json(rows);
+    } catch (err) {
+        
+    }
+}
