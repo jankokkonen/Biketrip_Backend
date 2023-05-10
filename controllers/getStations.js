04d56a70b33788ead8cddd255a1675f026a23e9f
jankokkonen <janne.kokkonen@edu.bc.fi>
2023-05-10 15:12:29 +0300

    Create controllers/getStations.js

---
+++ controllers/getStations.js
@@ -0,0 +1,14 @@
+const stationsModel = require('../models/getStation');
+
+const db = require('../db/database');
+
+exports.fetchTrips = async (req, res, next) => {
+    try {
+        const {rows} = await stationsModel.fetchStations();
+        console.log(rows);
+        res.status(200).json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
